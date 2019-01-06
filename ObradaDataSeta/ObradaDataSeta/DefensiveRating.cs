7c401ec7e46530fa4a91ce41652c01f09ea59245
djepeto3 <vukboskovic3@gmail.com>
2019-01-06 14:45:46 +0100

    Obrada dataseta

---
+++ ObradaDataSeta/ObradaDataSeta/DefensiveRating.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObradaDataSeta
+{
+    class DefensiveRating
+    {
+        public Dictionary<string, double> defensiveRatings1415 { get; set; }
+        public DefensiveRating()
+        {
+            defensiveRatings1415 = new Dictionary<string, double>();
+            defensiveRatings1415.Add("TOR", 100.9);
+            defensiveRatings1415.Add("BOS", 101.2);
+            defensiveRatings1415.Add("BKN", 100.9);
+            defensiveRatings1415.Add("PHI", 101);
+            defensiveRatings1415.Add("NYK", 101.2);
+            defensiveRatings1415.Add("CLE", 98.7);
+            defensiveRatings1415.Add("CHI", 97.8);
+            defensiveRatings1415.Add("MIL", 97.4);
+            defensiveRatings1415.Add("IND", 97);
+            defensiveRatings1415.Add("DET", 99.5);
+            defensiveRatings1415.Add("ATL", 97.1);
+            defensiveRatings1415.Add("WAS", 97.8);
+            defensiveRatings1415.Add("MIA", 97.3);
+            defensiveRatings1415.Add("CHA", 97.3);
+            defensiveRatings1415.Add("ORL", 101.4);
+            defensiveRatings1415.Add("POR", 98.6);
+            defensiveRatings1415.Add("OKC", 101.8);
+            defensiveRatings1415.Add("UTA", 94.9);
+            defensiveRatings1415.Add("DEN", 105);
+            defensiveRatings1415.Add("MIN", 106.5);
+            defensiveRatings1415.Add("GSW", 99.9);
+            defensiveRatings1415.Add("LAC", 100.1);
+            defensiveRatings1415.Add("PHX", 103.3);
+            defensiveRatings1415.Add("SAC", 105);
+            defensiveRatings1415.Add("LAL", 105.3);
+            defensiveRatings1415.Add("HOU", 100.5);
+            defensiveRatings1415.Add("SAS", 97);
+            defensiveRatings1415.Add("MEM", 95.1);
+            defensiveRatings1415.Add("DAL", 102.3);
+            defensiveRatings1415.Add("NOP", 98.6);
+        }
+
+        public double getTeamDefensiveRank1415(string name)
+        {
+
+            double rating = defensiveRatings1415[name];
+            return rating;
+            /*int position = 1;
+            foreach (var pair in defensiveRatings1415)
+            {
+                if (rating > pair.Value)
+                    position++;
+            }
+
+            return position;*/
+        }
+    }
+}
---
+++ ObradaDataSeta/ObradaDataSeta/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObradaDataSeta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] lines = File.ReadAllLines(@"./../../data/shot_logs.csv");
+            lines = lines.Skip(1).ToArray();
+            DefensiveRating defensiveRating = new DefensiveRating();            
+            List<List<string>> newLines = new List<List<string>>();
+            
+            foreach(string line in lines)
+            {
+                
+                string[] parameters = line.Split(',');
+                bool exists = new bool();
+                for(int i=newLines.Count-1; i>=0; i--)
+                {
+                    exists = false;
+                    List<string> chek = newLines[i];
+                    
+                    if (chek.Contains(parameters[0]) && chek.Contains(parameters[parameters.Length - 2]))
+                    {                        
+                        chek[8] = (int.Parse(chek[8])+ int.Parse(parameters[parameters.Length - 3])).ToString();
+                        exists = true;
+                        break;
+                    }
+                }
+                if (!exists)
+                {
+                    List<string> newLine = new List<string>();
+                    string pera = parameters[2];
+                    string teamName = "";
+                    teamName+=pera[pera.Length - 4];
+                    teamName += pera[pera.Length - 3];
+                    teamName += pera[pera.Length - 2];                   
+
+                    newLine.Add(parameters[0]);//game id
+                    newLine.Add(parameters[1] + "," + parameters[2]);//teams
+                    newLine.Add(parameters[3]); // location H/A
+                    newLine.Add(parameters[4]); // win W/L
+                    newLine.Add(parameters[5]); // final margin
+                    newLine.Add(defensiveRating.getTeamDefensiveRank1415(teamName).ToString()); // defensive rating
+                    newLine.Add(parameters[parameters.Length - 2]); //player name
+                    newLine.Add(parameters[parameters.Length - 1]); //player id
+                    newLine.Add(parameters[parameters.Length - 3].ToString());  //points
+                    newLines.Add(newLine);                   
+                }                
+            }
+            foreach (List<string> str in newLines)
+            {
+                foreach (string stri in str)
+                {
+                    Console.WriteLine(stri);
+                }
+            }
+            
+            string comma = ",";
+            string filePath = @"./../../data/dataSet.csv";
+            string[] columns = { "GAME_ID", "TEAMS", "LOCATION", "W/L", "FINAL_MARGIN", "DEFENSIVE_RATING", "PLAYER_NAME", "PLAYER_ID", "POINTS" };
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(string.Join(comma, columns));
+            File.WriteAllText(filePath, sb.ToString());
+
+            StringBuilder sb1 = new StringBuilder();
+            foreach(List<string> str in newLines)
+            {
+                sb1.AppendLine(string.Join(comma, str.ToArray()));
+            }
+            File.AppendAllText(filePath, sb1.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
